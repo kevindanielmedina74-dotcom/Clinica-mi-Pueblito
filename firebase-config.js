3e90663e0932299ea5accdb6223904bb9ec34141
kevin medina <perdomidany@gmail.com>
2026-06-28 14:32:28 -0600

    Firebase config real - Clinica Mi Pueblito

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,15 @@
 // ===== CONFIGURACION DE FIREBASE =====
-// 1. Ve a https://console.firebase.google.com
-// 2. Crea un proyecto
-// 3. Ve a "Project settings" > "Tus apps" > Web (</>)
-// 4. Copia el objeto firebaseConfig y pegalo abajo
-// 5. En Firestore Database, crea la BD en modo prueba
-
 const firebaseConfig = {
-  apiKey: "AQUI_TU_API_KEY",
-  authDomain: "AQUI_TU_PROJECT.firebaseapp.com",
-  projectId: "AQUI_TU_PROJECT_ID",
-  storageBucket: "AQUI_TU_PROJECT.appspot.com",
-  messagingSenderId: "AQUI_TU_SENDER_ID",
-  appId: "AQUI_TU_APP_ID"
+  apiKey: "AIzaSyD-n619sFipJG7suBudcF4XhZybG9M37Do",
+  authDomain: "clinica-mi-pueblito.firebaseapp.com",
+  projectId: "clinica-mi-pueblito",
+  storageBucket: "clinica-mi-pueblito.firebasestorage.app",
+  messagingSenderId: "453330398519",
+  appId: "1:453330398519:web:ce8de6406f94af1cc73eb2",
+  measurementId: "G-BKL9JFVJ4E"
 };
 
-let _db = null;
+var _db = null;
 try {
   firebase.initializeApp(firebaseConfig);
   _db = firebase.firestore();
